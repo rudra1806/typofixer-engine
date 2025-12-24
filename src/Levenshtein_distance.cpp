71d5fb0f3bd05b2a800ca4cfaa63b9c4e1efd059
Rudra Sanandiya <rudrasanandiyards@gmail.com>
2025-12-24 18:40:25 +0530

    solved the out of bound error in Levenshtein_distance.cpp file

--- src/Levenshtein_distance.cpp
+++ src/Levenshtein_distance.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int levenshtein_distance(const string &input,const string &target){
     const int m=target.size(); // length of target string
     const int n=input.size(); // length of input string
-    vector<vector<int>> ld(m+1,vector<int>(n+1,0)); // 2D vector for distance matrix
-    for(int i=0;i<n+1;i++) ld[0][i]=i;
-    for(int i=0;i<m+1;i++) ld[i][0]=i;
+    vector<vector<int>> ld(n+1,vector<int>(m+1,0)); // 2D vector for distance matrix
+    for(int i=0;i<m+1;i++) ld[0][i]=i;
+    for(int i=0;i<n+1;i++) ld[i][0]=i;
     for(int i=1;i<n+1;i++){
         for(int j=1;j<m+1;j++){
             if(input[i-1]==target[j-1]){
